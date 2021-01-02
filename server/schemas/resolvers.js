d4afa8cd0708171e452009c2cfb65cce5a9d43a6
Matthew O <mo@pop-os.localdomain>
2021-01-02 13:28:31 -0700

    added apollo server

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,7 @@
+const { AuthenticationError } = require("apollo-server-express");
+
+const resolvers = {
+  // code
+};
+
+module.exports = resolvers;
--- server/server.js
+++ server/server.js
@@ -1,21 +1,40 @@
-const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
+const express = require("express");
+const path = require("path");
+const db = require("./config/connection");
+//import typedefs and resolvers
+const { ApolloServer } = require("apollo-server-express");
+const { typeDefs, resolvers } = require("./schemas");
+const { authMiddleware } = require("./utils/auth");
 
+// express init
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Apollo init
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
+
+// integrate Apollo server with express
+server.applyMiddleware({ app });
+
+// Express config
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-app.use(routes);
+// fallback route
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
-db.once('open', () => {
+// server listener
+db.once("open", () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
